f4e7fe9885a8d793477cf0ce40b1cd5fa9facebd
Tobias K <nox@raynode.de>
2019-05-27 10:22:03 +0200

    added readme for basic context handling

--- src/example/context-1.spec.ts
+++ src/example/context-1.spec.ts
@@ -18,41 +18,23 @@ interface UserAction extends NodeType {
 
 type SchemaTypes = 'admin' | 'user'
 
-const Users: Service<User> = {
-  findMany: async () => ({
-    page: { limit: 100, offset: 0 },
-    nodes: [],
-  }),
-  // findOne: async () => null,
-  // create: async () => null,
-  // remove: async () => null,
-  // update: async () => null,
-}
-
 describe('testing the example 1', () => {
   it('should build the example code', async () => {
-    const builder = createSchemaBuilder<SchemaTypes>()
-    const user = builder.model('User', Users)
-    user.context((model, { context }) => {
-      console.log('checking for context: ', context)
-      if (context === 'user') model.visibility.findManyQuery = false
-    })
-
-    user.attr('email', GraphQLString)
-
-    builder.addType('context', GraphQLString, ({ context }) => {
-      console.log(context)
-      return () => context
-    })
     interface GQLContext {
       authToken?: string
       authId?: string
     }
+
+    const builder = createSchemaBuilder<SchemaTypes>()
+    const user = builder.model('User')
+    user.attr('email', GraphQLString)
+
+    builder.addType('context', GraphQLString, ({ context }) => () => context)
+
     builder.addType(
       'me',
       user,
       ({ context: schemaContext }) => (root, args, context?: GQLContext) => {
-        console.log('trying to get ME', context)
         if (!context) throw new Error('Need an authToken-context')
         if (schemaContext === 'user' && !context.authToken)
           throw new Error('Need an user:authToken')
@@ -66,23 +48,33 @@ describe('testing the example 1', () => {
 
     const adminSchema = builder.build('admin')
     const userSchema = builder.build('user')
+
+    const source = `{ context me { email }}`
     // console.log(printSchema(adminSchema))
     // console.log(printSchema(userSchema))
     const adminResult = await graphql({
       schema: adminSchema,
-      source: `{ context me { email }}`,
+      source,
       contextValue: {
         authId: 'AdminID',
       },
     })
     const userResult = await graphql({
       schema: userSchema,
-      source: `{ context me { email }}`,
+      source,
       contextValue: {
         authId: 'AuthenticationID',
         authToken: 'My@Token.com',
       },
     })
-    console.log(adminResult, userResult)
+    expect(userResult.data).toEqual({
+      context: 'user',
+      me: { email: 'My@Token.com' },
+    })
+    expect(adminResult.data).toEqual({
+      context: 'admin',
+      me: { email: 'admin' },
+    })
+    expect(printSchema(userSchema)).toMatchSnapshot()
   })
 })
